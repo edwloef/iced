7843e522f1684afa33274d3c8f881b2eed0d6a26
edwloef <edwin.frank.loeffler@gmail.com>
2026-08-10 12:58:13 +0200

    fix `toggler` widget with `crisp` disabled [no ci]

--- widget/src/toggler.rs
+++ widget/src/toggler.rs
@@ -442,7 +442,7 @@ where
         let toggle_bounds = {
             // Try to align toggle to the pixel grid
             let bounds = if renderer::CRISP {
-                (bounds * scale_factor).round()
+                (bounds * scale_factor).round() * (1.0 / scale_factor)
             } else {
                 bounds
             };
@@ -459,7 +459,7 @@ where
                 y: bounds.y + padding,
                 width: bounds.height - (2.0 * padding),
                 height: bounds.height - (2.0 * padding),
-            } * (1.0 / scale_factor)
+            }
         };
 
         renderer.fill_quad(
